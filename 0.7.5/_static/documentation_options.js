c3b3919af8f1d11f5f155248ca48f2b1e9ac8822
Aaron Meurer <asmeurer@gmail.com>
2020-01-31 17:23:56 -0700

    Generate "latest" redirects for all the old versions that used to be served
    They won't be accurate if things have moved around, but this is as good as
    it's going to get.

---
+++ 0.7.5/_static/documentation_options.js
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <meta http-equiv="refresh" content="0;URL=/latest/_static/documentation_options.js" />
+    </head>
+    <body>
+    </body>
+</html>
+
---
+++ 1.1/searchindex.js
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <meta http-equiv="refresh" content="0;URL=/latest/searchindex.js" />
+    </head>
+    <body>
+    </body>
+</html>
+
