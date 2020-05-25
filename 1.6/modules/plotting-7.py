88354a9b23865abb4334325e080e00ec9fbcae56
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2020-05-25 22:29:58 +0100

    Add sympy 1.6 docs

---
+++ 1.6/modules/plotting-7.py
@@ -0,0 +1,3 @@
+plot(x**2, adaptive=False, nb_of_points=400)
+# Plot object containing:
+# [0]: cartesian line: x**2 for x over (-10.0, 10.0)
---
+++ latest/modules/plotting-32.py
@@ -0,0 +1,9 @@
+PlotGrid(1, 3 , p2, p3, p4)
+# PlotGrid object containing:
+# Plot[0]:Plot object containing:
+# [0]: cartesian line: x**2 for x over (-6.0, 6.0)
+# [1]: cartesian line: x for x over (-5.0, 5.0)
+# Plot[1]:Plot object containing:
+# [0]: cartesian line: x**3 for x over (-5.0, 5.0)
+# Plot[2]:Plot object containing:
+# [0]: cartesian surface: x*y for x over (-5.0, 5.0) and y over (-5.0, 5.0)
