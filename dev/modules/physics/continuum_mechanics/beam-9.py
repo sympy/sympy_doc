e44c646d1d037e89623263fbf1ba31b9e406ac64
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2021-07-03 08:57:01 +0000

    Update docs after building Travis build 46181 of sympy/sympy
    The docs were built from the branch 'master' against the commit
    b2bbfed5f918bcd1eb4b1b4b4e5e8d8673c04c4e.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/sympy/sympy/jobs/774654110.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.8.7/bin/doctr deploy dev --deploy-repo sympy/sympy_doc --command ./generate_indexes.py

--- dev/modules/physics/continuum_mechanics/beam-9.py
+++ dev/modules/physics/continuum_mechanics/beam-9.py
@@ -2,6 +2,7 @@ from sympy.physics.continuum_mechanics.beam import Beam3D
 from sympy import symbols
 l, E, G, I, A, x = symbols('l, E, G, I, A, x')
 b = Beam3D(20, E, G, I, A, x)
+subs = {E:40, G:21, I:100, A:25}
 b.apply_load(15, start=0, order=0, dir="z")
 b.apply_load(12*x, start=0, order=0, dir="y")
 b.bc_deflection = [(0, [0, 0, 0]), (20, [0, 0, 0])]
@@ -11,11 +12,14 @@ b.apply_load(R2, start=20, order=-1, dir="z")
 b.apply_load(R3, start=0, order=-1, dir="y")
 b.apply_load(R4, start=20, order=-1, dir="y")
 b.solve_for_reaction_loads(R1, R2, R3, R4)
-b.plot_shear_force()
+b.solve_slope_deflection()
+b.plot_loading_results('y',subs)
 # PlotGrid object containing:
 # Plot[0]:Plot object containing:
-# [0]: cartesian line: 0 for x over (0.0, 20.0)
-# Plot[1]:Plot object containing:
 # [0]: cartesian line: -6*x**2 for x over (0.0, 20.0)
+# Plot[1]:Plot object containing:
+# [0]: cartesian line: -15*x**2/2 for x over (0.0, 20.0)
 # Plot[2]:Plot object containing:
-# [0]: cartesian line: -15*x for x over (0.0, 20.0)
+# [0]: cartesian line: x**2*(150 - 5*x)/8000 - x/8 for x over (0.0, 20.0)
+# Plot[3]:Plot object containing:
+# [0]: cartesian line: x*(105*x**4 - 8026000*x**2/43 + 109200000*x/43 + 304000000/43)/4200000 for x over (0.0, 20.0)
