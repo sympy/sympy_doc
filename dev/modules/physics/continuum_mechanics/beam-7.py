0691bbc3cc447dbaa045d8b6189b15f57c1cbd22
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2021-08-01 17:40:34 +0000

    Update docs after building Travis build 46225 of sympy/sympy
    The docs were built from the branch 'master' against the commit
    d1ea82c7611578faa14ccda9a6277aae91983493.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/sympy/sympy/jobs/774692009.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.8.7/bin/doctr deploy dev --deploy-repo sympy/sympy_doc --command ./generate_indexes.py

--- dev/modules/physics/continuum_mechanics/beam-7.py
+++ dev/modules/physics/continuum_mechanics/beam-7.py
@@ -1,21 +1,15 @@
-from sympy.physics.continuum_mechanics.beam import Beam3D
+from sympy.physics.continuum_mechanics.beam import Beam
 from sympy import symbols
-l, E, G, I, A, x = symbols('l, E, G, I, A, x')
-b = Beam3D(20, E, G, I, A, x)
-b.apply_load(15, start=0, order=0, dir="z")
-b.apply_load(12*x, start=0, order=0, dir="y")
-b.bc_deflection = [(0, [0, 0, 0]), (20, [0, 0, 0])]
-R1, R2, R3, R4 = symbols('R1, R2, R3, R4')
-b.apply_load(R1, start=0, order=-1, dir="z")
-b.apply_load(R2, start=20, order=-1, dir="z")
-b.apply_load(R3, start=0, order=-1, dir="y")
-b.apply_load(R4, start=20, order=-1, dir="y")
-b.solve_for_reaction_loads(R1, R2, R3, R4)
-b.plot_bending_moment()
-# PlotGrid object containing:
-# Plot[0]:Plot object containing:
-# [0]: cartesian line: 0 for x over (0.0, 20.0)
-# Plot[1]:Plot object containing:
-# [0]: cartesian line: -15*x**2/2 for x over (0.0, 20.0)
-# Plot[2]:Plot object containing:
-# [0]: cartesian line: 2*x**3 for x over (0.0, 20.0)
+R1, R2 = symbols('R1, R2')
+b = Beam(8, 200*(10**9), 400*(10**-6))
+b.apply_load(5000, 2, -1)
+b.apply_load(R1, 0, -1)
+b.apply_load(R2, 8, -1)
+b.apply_load(10000, 4, 0, end=8)
+b.bc_deflection = [(0, 0), (8, 0)]
+b.solve_for_reaction_loads(R1, R2)
+b.plot_slope()
+# Plot object containing:
+# [0]: cartesian line: -8.59375e-5*SingularityFunction(x, 0, 2) + 3.125e-5*SingularityFunction(x, 2, 2)
+# + 2.08333333333333e-5*SingularityFunction(x, 4, 3) - 0.0001953125*SingularityFunction(x, 8, 2)
+# - 2.08333333333333e-5*SingularityFunction(x, 8, 3) + 0.00138541666666667 for x over (0.0, 8.0)
