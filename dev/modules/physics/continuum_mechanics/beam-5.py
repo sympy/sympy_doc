83f594c64db1f6da599e8853d770a50a8aff0e9d
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2021-08-10 18:54:30 +0000

    Update docs after building Travis build 46236 of sympy/sympy
    The docs were built from the branch 'master' against the commit
    782b80f3a6001cbb318096df5f13840b221be6ea.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/sympy/sympy/jobs/774705507.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.8.7/bin/doctr deploy dev --deploy-repo sympy/sympy_doc --command ./generate_indexes.py

--- dev/modules/physics/continuum_mechanics/beam-5.py
+++ dev/modules/physics/continuum_mechanics/beam-5.py
@@ -1,11 +1,14 @@
-from sympy.physics.continuum_mechanics.beam import Beam
 from sympy import symbols
-R1, R2 = symbols('R1, R2')
-b = Beam(8, 200*(10**9), 400*(10**-6))
-b.apply_load(5000, 2, -1)
-b.apply_load(R1, 0, -1)
-b.apply_load(R2, 8, -1)
-b.apply_load(10000, 4, 0, end=8)
-b.bc_deflection = [(0, 0), (8, 0)]
-b.solve_for_reaction_loads(R1, R2)
-axes = b.plot_loading_results()
+from sympy.physics.continuum_mechanics.beam import Beam
+E, I = symbols('E, I')
+R_0, R_8 = symbols('R_0, R_8')
+b = Beam(12, E, I)
+b.apply_support(0, 'roller')
+b.apply_support(8, 'roller')
+b.solve_for_ild_reactions(1, R_0, R_8)
+b.solve_for_ild_shear(4, 1, R_0, R_8)
+b.ild_shear
+# Piecewise((x/8, x < 4), (x/8 - 1, x > 4))
+b.plot_ild_shear()
+# Plot object containing:
+# [0]: cartesian line: Piecewise((x/8, x < 4), (x/8 - 1, x > 4)) for x over (0.0, 12.0)
