987d43cc771d5c808292baa3592d7441ebf7a4d9
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2022-01-30 12:09:07 +0000

    Update docs after building Travis build 46718 of sympy/sympy
    The docs were built from the branch 'master' against the commit
    457e5ec5ff7149cec89f3a3d1cf364d14fe9c466.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/sympy/sympy/jobs/774782215.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.8.12/bin/doctr deploy dev --deploy-repo sympy/sympy_doc --command ./generate_indexes.py

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.10.dev',
+    VERSION: '1.11.dev',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
